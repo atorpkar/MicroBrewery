01775bcb0d6eaba60a5e508f39c984a236025938
glasspaper <glasspaper@WS510-404>
2017-08-22 16:56:46 +0200

    Added a new web application after adding NuGet packages that failed the previous app

---
+++ MicroBreweryWebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MicroBreweryWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // private BreweryServices breweryService; 
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult User()
+        {
+            ViewBag.Message = "USER PAGE";
+
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            ViewBag.Message = "ADMIN PAGE";
+
+            return View();
+        }
+    }
+}
---
+++ MicroBreweryWebApplication2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MicroBreweryWebApplication2.MvcApplication" Language="C#" %>
